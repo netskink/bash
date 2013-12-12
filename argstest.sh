38f5856e3cbef467795ad4f954007aa7742c35f0
netskink <davisjf@gmail.com>
2013-12-12 12:19:11 -0800

    WTF was I thinking.

--- argstest.sh
+++ argstest.sh
@@ -5,18 +5,15 @@
 # $@ represents all arguments
 # $# is the count of arguments
 
-#echo ${args[0]} ${args[1]} ${args[2]} 
-
-#for (( expr; expr; expr )); do COMMANDS; done # Each expr is done in a math context. Example: for ((i=1; i<=n; 
-#                 i++)); do echo "$i"; done. Use in place of the non-working: for i in {1..$number}
 
 echo "Number of args is $#"
 
 if [ $# -gt 0 ]; then
 	echo "args are $@"
 	args=("$@")
-	for ((i=1; i<=$#; i++));
+	for ((i=0; i<$#; i++));
 	do
-		echo "arg $i is ${args[$i-1]}";
+		# the args list is zero based
+		echo "arg $i is ${args[$i]}";
 	done
 fi
